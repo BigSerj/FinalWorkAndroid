41b9b5c284b5a2814d6afd8e935eb9f22f6df4b9
BigSerj <sergeyconvent@gmail.com>
2017-08-11 18:33:15 +0300

    FinalWorkAndroid

--- WeatherForecastLSA/app/src/main/java/com/android/bigserj/weatherforecastlsa/presentation/view/activity/MainActivity.java
+++ WeatherForecastLSA/app/src/main/java/com/android/bigserj/weatherforecastlsa/presentation/view/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.android.bigserj.weatherforecastlsa;
+package com.android.bigserj.weatherforecastlsa.presentation.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.android.bigserj.weatherforecastlsa.R;
 
 public class MainActivity extends AppCompatActivity {
 
